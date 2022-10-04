10d30696faaa4199e0e905fa62a9849cb243888c
FADHILAKRAM <fadhilakram758@gmail.com>
2022-10-04 14:55:21 +0700

    PRAKTIKUM

---
+++ PRAKTIKUM 3 Array, Fungsi dan Modularisasi/Array_asosiatif.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $telpon["FADHIL"] = "089529312698";
+        $telpon["RISKI"] = "089674321256";
+        $telpon["ZAKI"] = "08953214567";
+        echo"telpon FADHIL :".$telpon['FADHIL'];
+        echo"<br>";
+        echo"telpon RISKI :".$telpon['RISKI'];
+        echo"<br>";
+        echo"telpon ZAKI :".$telpon['ZAKI'];
+        echo"<br>";
+        ?>
+</body>
+</html>
